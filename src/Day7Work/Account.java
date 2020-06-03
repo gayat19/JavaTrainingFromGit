89dbeee711d284d3f6b3dd70817830c2ac8c842b
unknown <gayat19>
2020-06-03 11:46:23 +0530

    Day 7 works example

---
+++ src/Day7Work/Account.java
@@ -0,0 +1,32 @@
+package Day7Work;
+
+public class Account {
+
+	String number,name;
+	double balance;
+	
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	Account(){
+		System.out.println("This is a new account");
+	}
+	
+	
+}
